3e9442c6eff54d58105d83ecfadf661dab832148
Valentin Greiner <valentin.greiner04@gmail.com>
2022-06-30 00:30:12 +0200

    initial commit

---
+++ 06_LINQ - Datenbank/231_Musterloesung LINQ selbst programmiert.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqLib
+{
+  public static class MyLinqExtensions
+  {
+
+    #region ---------------------------------------- Filter
+
+    public delegate bool MyFilter<T>(T item);
+    public static IEnumerable<T> Where<T>(
+      this IEnumerable<T> cltn,
+      MyFilter<T> filter)
+    {
+      foreach (T item in cltn)
+      {
+        if (filter(item)) yield return item;
+      }
+    }
+
+    public static IEnumerable<T> Distinct<T>(this IEnumerable<T> cltn)
+    {
+      var uniques = new HashSet<T>();
+      foreach (var item in cltn)
+      {
+        uniques.Add(item);
+      }
+      foreach (var item in uniques) yield return item;
+    }
+
+    public static IEnumerable<T> Take<T>(this IEnumerable<T> cltn, int nr)
+    {
+      int count = 1;
+      foreach (var item in cltn)
+      {
+        if (count > nr) break;
+        yield return item;
+        count++;
+      }
+    }
+
+    public static IEnumerable<T> TakeWhile<T>(this IEnumerable<T> cltn, Predicate<T> filter)
+    {
+      foreach (T item in cltn)
+      {
+        if (filter(item) == false) break;
+        yield return item;
+      }
+    }
+
+    public static IEnumerable<T> Skip<T>(this IEnumerable<T> cltn, int nr)
+    {
+      int count = 1;
+      foreach (var item in cltn)
+      {
+        if (count > nr) yield return item;
+        count++;
+      }
+    }
+
+    public static IEnumerable<T> SkipWhile<T>(this IEnumerable<T> cltn, Predicate<T> filter)
+    {
+      bool doTrySkip = true;
+      foreach (T item in cltn)
+      {
+        if (doTrySkip && filter(item)) continue;
+        doTrySkip = false;
+        yield return item;
+      }
+    }
+
+    #endregion
+
+    #region ---------------------------------------- Projection
+    public static IEnumerable<S> Select<T, S>(this IEnumerable<T> cltn, Func<T, S> transformer)
+    {
+      List<S> ret = new();
+      foreach (var item in cltn)
+      {
+        ret.Add(transformer(item));
+      }
+      return ret;
+    }
+    #endregion
+
+    #region ---------------------------------------- First/Last
+    public static T First<T>(this IEnumerable<T> cltn)
+    {
+      foreach (var item in cltn)
+      {
+        return item;
+      }
+      throw new Exception("Sequence contains no elements");
+    }
+
+    public static T FirstOrDefault<T>(this IEnumerable<T> cltn)
+    {
+      foreach (var item in cltn)
+      {
+        return item;
+      }
+      return default(T);
+    }
+
+    public static T Last<T>(this IEnumerable<T> cltn)
+    {
+      if (!cltn.Any()) throw new Exception("Sequence contains no elements");
+      T last = default(T);
+      foreach (var item in cltn)
+      {
+        last = item;
+      }
+      return last;
+    }
+
+    public static T LastOrDefault<T>(this IEnumerable<T> cltn)
+    {
+      if (!cltn.Any()) return default(T);
+      T last = default(T);
+      foreach (var item in cltn)
+      {
+        last = item;
+      }
+      return last;
+    }
+
+    public static T First<T>(this IEnumerable<T> cltn, Predicate<T> condition)
+    {
+      foreach (var item in cltn)
+      {
+        if (condition(item)) return item;
+      }
+      throw new Exception("Sequence contains no elements with specified condition");
+    }
+
+    public static T Last<T>(this IEnumerable<T> cltn, Predicate<T> condition)
+    {
+      T last = default(T);
+      bool found = false;
+      foreach (var item in cltn)
+      {
+        if (!condition(item)) continue;
+        last = item;
+        found = true;
+      }
+      if (found) return last;
+      throw new Exception("Sequence contains no elements with specified condition");
+    }
+
+    public static T FirstOrDefault<T>(this IEnumerable<T> cltn, Predicate<T> condition)
+    {
+      foreach (var item in cltn)
+      {
+        if (condition(item)) return item;
+      }
+      return default(T);
+    }
+
+    public static T LastOrDefault<T>(this IEnumerable<T> cltn, Predicate<T> condition)
+    {
+      T last = default(T);
+      foreach (var item in cltn)
+      {
+        if (condition(item)) last = item;
+      }
+      return last;
+    }
+
+    #endregion
+
+    #region ---------------------------------------- Aggregation
+    public static double Average(this IEnumerable<int> cltn)
+    {
+      double sum = 0.0;
+      int count = 0;
+      foreach (var item in cltn)
+      {
+        sum += item;
+        count++;
+      }
+      return count == 0 ? 0 : sum / count;
+    }
+    public static double Average(this IEnumerable<double> cltn)
+    {
+      double sum = 0;
+      int count = 0;
+      foreach (var item in cltn)
+      {
+        sum += item;
+        count++;
+      }
+      return count == 0 ? 0 : sum / count;
+    }
+    public static int Average<T>(this IEnumerable<T> cltn, Func<T, int> f)
+    {
+      int sum = 0;
+      int count = 0;
+      foreach (var item in cltn)
+      {
+        sum += f(item);
+        count++;
+      }
+      return count == 0 ? 0 : sum / count;
+    }
+    public static double Average<T>(this IEnumerable<T> cltn, Func<T, double> f)
+    {
+      double sum = 0;
+      int count = 0;
+      foreach (var item in cltn)
+      {
+        sum += f(item);
+        count++;
+      }
+      return count == 0 ? 0 : sum / count;
+    }
+
+    public static int Max(this IEnumerable<int> cltn)
+    {
+      if (cltn.Count() == 0) throw new Exception("Sequence contains no elements");
+      int max = -int.MaxValue;
+      foreach (var item in cltn)
+      {
+        if (item > max) max = item;
+      }
+      return max;
+    }
+    public static double Max(this IEnumerable<double> cltn)
+    {
+      if (cltn.Count() == 0) throw new Exception("Sequence contains no elements");
+      double max = -double.MaxValue;
+      foreach (var item in cltn)
+      {
+        if (item > max) max = item;
+      }
+      return max;
+    }
+
+    public static int Count<T>(this IEnumerable<T> cltn)
+    {
+      int count = 0;
+      foreach (var item in cltn)
+      {
+        count++;
+      }
+      return count;
+    }
+    public static int Count<T>(this IEnumerable<T> cltn, Predicate<T> filter)
+    {
+      int count = 0;
+      foreach (var item in cltn)
+      {
+        if (filter(item)) count++;
+      }
+      return count;
+    }
+    public static bool Any<T>(this IEnumerable<T> cltn)
+    {
+      return cltn.Count() > 0;
+    }
+
+    #endregion
+
+    #region ---------------------------------------- Sorting
+    public static IEnumerable<T> OrderBy<T, T_out>(
+      this List<T> cltn,
+      Func<T, T_out> f) where T_out : IComparable
+    {
+      cltn.Sort((a, b) => f(a).CompareTo(f(b)));
+      return cltn;
+    }
+    //public static IEnumerable<T> OrderBy<T>(this List<T> cltn, Func<T, int> f)
+    //{
+    //  Comparison<T> comparer = (a, b) =>
+    //  {
+    //    int x = f(a).CompareTo(f(b));
+    //    return x;
+    //    //return f(a) - f(b);
+    //    //if (f(a) == f(b)) return 0;
+    //    //if (f(a) > f(b)) return 1;
+    //    //return -1;
+    //  };
+    //  cltn.Sort(comparer);
+    //  return cltn;
+    //}
+    //public static IEnumerable<T> OrderBy<T>(this List<T> cltn, Func<T, double> f)
+    //{
+
+    //  Comparison<T> comparer = (a, b) =>
+    //  {
+    //    int x = f(a).CompareTo(f(b));
+    //    return x;
+    //  };
+    //  cltn.Sort(comparer);
+    //  return cltn;
+    //}
+    public static IEnumerable<T> OrderByDescending<T>(this List<T> cltn, Func<T, int> f)
+    {
+      Comparison<T> comparer = (a, b) =>
+      {
+        int x = f(b).CompareTo(f(a));
+        return x;
+      };
+      cltn.Sort(comparer);
+      return cltn;
+    }
+    public static IEnumerable<T> OrderByDescending<T>(this List<T> cltn, Func<T, double> f)
+    {
+      Comparison<T> comparer = (a, b) =>
+      {
+        int x = f(b).CompareTo(f(a));
+        return x;
+      };
+      cltn.Sort(comparer);
+      return cltn;
+    }
+
+    public static IEnumerable<T> Reverse<T>(this IEnumerable<T> cltn)
+    {
+      var ret = new List<T>();
+      foreach (var item in cltn)
+      {
+        ret.Insert(0, item);
+      }
+      return ret;
+    }
+    #endregion
+
+    #region ---------------------------------------- Conversion
+    public static List<T> ToList<T>(this IEnumerable<T> cltn)
+    {
+      var list = new List<T>();
+      foreach (var item in cltn)
+      {
+        list.Add(item);
+      }
+      return list;
+    }
+
+    public static T[] ToArray<T>(this IEnumerable<T> cltn)
+    {
+      var array = new T[cltn.Count()];
+      int i = 0;
+      foreach (var item in cltn)
+      {
+        array[i] = item;
+      }
+      return array;
+    }
+    #endregion
+
+  }
+}
---
+++ 06_LINQ - Datenbank/322_Musterlösung_HÜ_Northwind_LINQ_Queries.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NorthwindDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NorthwindLinq
+{
+  public class Queries
+  {
+    private NorthwindContext db;
+    public Queries()
+    {
+      InitializeDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+      var config = new ConfigurationBuilder()
+         .SetBasePath(AppContext.BaseDirectory)
+         .AddJsonFile("appsettings.json")
+         .Build();
+      var connectionString = config.GetConnectionString("NorthwindDb");
+      var options = new DbContextOptionsBuilder<NorthwindContext>()
+                        //.LogTo(Console.WriteLine, LogLevel.Information)
+                        .UseSqlServer(connectionString)
+                        .Options;
+      db = new NorthwindContext(options);
+    }
+
+    public void CheckAll()
+    {
+      First5ProductsWithCategories().Show("First5ProductsWithCategories");
+      OrderedProductsOfBOTTM().Show("OrderedProductsOfBOTTM");
+      NrOfEmployeesWhoSoldToCustomersInGivenCity("Nantes").Show("NrOfEmployeesWhoSoldToCustomersInGivenCity Nantes");
+      NrOfEmployeesWhoSoldToCustomersInGivenCity("London").Show("NrOfEmployeesWhoSoldToCustomersInGivenCity London");
+      NrOfEmployeesWhoSoldToCustomersInGivenCity("Buenos Aires").Show("NrOfEmployeesWhoSoldToCustomersInGivenCity Buenos Aires");
+      CustomersWithUnshippedOrders().Show("CustomersWithUnshippedOrders");
+      TotalQuantityOfShipper("Speedy Express").Show("TotalQuantityOfShipper Speedy Express");
+      TotalQuantityOfShipper("United Package").Show("TotalQuantityOfShipper United Package");
+      TotalQuantityOfShipper("Federal Shipping").Show("TotalQuantityOfShipper Federal Shipping");
+      AveragePriceOfSuppliersOfCity("Tokyo").Show("AveragePriceOfSuppliersOfCity Tokyo");
+      AveragePriceOfSuppliersOfCity("Paris").Show("AveragePriceOfSuppliersOfCity Paris");
+      AveragePriceOfSuppliersOfCity("Berlin").Show("AveragePriceOfSuppliersOfCity Berlin");
+      CategoriesWithProductsInStockMoreThan(400).Show("CategoriesWithProductsInStockMoreThan 400");
+      CategoriesWithProductsInStockMoreThan(600).Show("CategoriesWithProductsInStockMoreThan 600");
+      CategoriesWithProductsInStockMoreThan(200).Show("CategoriesWithProductsInStockMoreThan 200");
+    }
+
+    #region Q01
+    public List<string> First5ProductsWithCategories()
+    {
+      try
+      {
+        return db.Products
+          .OrderBy(x => x.ProductName)
+          .Select(x => new { x.ProductName, x.Category.CategoryName })
+          .Take(5)
+          .ToList() //to be able to use interpolated strings
+          .Select(x => $"{x.ProductName} - {x.CategoryName}")
+          .ToList();
+      }
+      catch (Exception exc)
+      {
+        Console.WriteLine(exc.ToString());
+      }
+      return new List<string>();
+    }
+    #endregion
+
+    #region Q02
+    public List<string> OrderedProductsOfBOTTM()
+    {
+      return db.OrderDetails
+        .Where(x => x.Order.CustomerId == "BOTTM" && x.Product.UnitPrice > 30)
+        .Select(x => x.Product.ProductName)
+        .Distinct()
+        .OrderBy(x => x).ToList();
+    }
+    #endregion
+
+    #region Q03
+    public int NrOfEmployeesWhoSoldToCustomersInGivenCity(string city)
+    {
+      return db.Orders
+         .Where(x => x.Customer.City == city)
+         .Select(x => x.Employee.LastName)
+         .Distinct()
+         .Count();
+    }
+    #endregion
+
+    #region Q04
+    public List<string> CustomersWithUnshippedOrders()
+    {
+      return db.Orders
+        .Include(x => x.Customer)
+        .Include(x => x.Employee)
+        .Where(x => x.OrderDate != null && x.ShippedDate == null
+              && (x.Customer.Country == "Venezuela" || x.Customer.Country == "Argentina"))
+        .OrderBy(x => x.Customer.CompanyName)
+        .ThenBy(x => x.Employee.LastName)
+        .ToList()
+        .Select(x => $"{x.Customer.CompanyName} - {x.Customer.City}/{x.Customer.Country} - {x.Employee.FirstName} {x.Employee.LastName}")
+        .Distinct()
+        .ToList();
+    }
+    #endregion
+
+    #region Q05
+    public int TotalQuantityOfShipper(string shipperCompany)
+    {
+      return db.OrderDetails
+        .Where(x => x.Order.ShipViaNavigation.CompanyName == shipperCompany && x.Order.ShippedDate != null)
+        .Select(x => (int)x.Quantity)
+        .Sum();
+    }
+    #endregion
+
+    #region Q06
+    public double AveragePriceOfSuppliersOfCity(string city)
+    {
+      return db.Products
+        .Where(x => x.Supplier.City == city)
+        .Select(x => (double)x.UnitPrice)
+        .Average();
+    }
+    #endregion
+
+    #region Q07
+    public List<string> CategoriesWithProductsInStockMoreThan(int totalStock)
+    {
+      //var q_ = from p in db.Products
+      //        join c in db.Categories
+      //        on p.CategoryID equals c.CategoryID
+      //        group (int)p.UnitsInStock by c.CategoryName;
+      var q = db.Products
+        .Include(x => x.Category)
+        .AsEnumerable()
+        .GroupBy(x => x.Category.CategoryName, x => (int)x.UnitsInStock);
+      var st = q.ToDictionary(x => x.Key, x => x.Sum());
+      return st.Where(x => x.Value > totalStock)
+               .Select(x => x.Key)
+               .OrderBy(x => x)
+               .ToList();
+    }
+    #endregion
+
+  }
+}
